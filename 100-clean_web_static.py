c9094fc099c5a31996d1ac1ffa9fbacbab481064
Serah Njoroge <105037236+njoroge-s@users.noreply.github.com>
2022-12-21 17:09:40 +0300

    Update 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ['34.138.32.248', '3.226.74.205']
+env.hosts = ['54.236.27.203', '35.153.17.253']
 
 
 @runs_once
